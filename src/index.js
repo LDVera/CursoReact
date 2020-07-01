fd9d3a4dcfa8dd765c46506ead34d58fba971802
Diego <ldvera41@gmail.com>
2020-06-30 19:03:21 -0500

    JFX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+const element =  document.createElement('h1')
+const contenedor = document.getElementById('root')
+element.innerText = 'Hola mundo'
+contenedor.appendChild(element)
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*AREA DE VARIABLES*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const nombre = 'diego';
+const user = {
+  firstname: 'juan',
+  lastname: 'perez',
+  avatar: 'https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'
+}
+const element = <h1>hola mundo con react soy {getNmae(user)} </h1>
+const element2 = <div> {userValidate(user)} </div> 
+const element3 = <img src={user.avatar} />
+const prueba = (
+  <div>
+    <div> {userValidate(user)} </div> 
+    <img src={user.avatar} />
+  </div>
+)
+const container = document.getElementById('root')
+
+/*FIN DE AREA DE VARIABLES*/
+
+/*AREA DE FUNCIONES*/
+function getNmae (user){
+  return user.firstname + " " + user.lastname
+}
+function userValidate (user){
+  if(user){
+    return <h1> bienvenido {getNmae(user)}</h1>
+  }
+    return <h1> Bienvenido desconocido</h1> 
+}
+/*FIN DE AREA DE FUNCIONES*/
+//ReactDOM.render(__QUE__,__DONDE__ )
+ReactDOM.render(prueba, container)
+
